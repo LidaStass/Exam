f76efec5357a75f17bbfd3da7bf691e3d56dd909
Lida Stass <lidastass@gmail.com>
2017-06-16 18:06:35 +0300

    Com1

---
+++ src/main/java/com/duck/proj/NewClass.java
@@ -0,0 +1,11 @@
+package com.duck.proj;
+
+/**
+ * Created by Admin on 15.06.2017.
+ */
+public class NewClass {
+    public void newMethod(){
+
+
+    }
+}
---
+++ src/main/java/com/duck/proj/duckProj.java
@@ -0,0 +1,34 @@
+package com.duck.proj;
+
+import org.junit.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
+
+public class duckProj extends DuckBase {
+
+    @Test
+    public void duckExam() {
+        System.setProperty("selenide.browser", "Chrome");
+        //вызываю браузер chrome
+        open("http://litecart.com/en/");
+        //подключила библиотеку selenide.open, откріваю сайт litecard.com
+
+        CreateNewAccount();
+
+        CurrencyChange();
+
+        GreenDuck();
+
+        PurpleDuck();
+
+        ShoppingCartConfirmOrder();
+
+        HomeButton();
+
+        Logout();
+
+    }
+
+}
+
